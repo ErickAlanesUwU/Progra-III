40dc7ee353efab9b5852ca8c9c5e6510f417b6b2
ErickAlanesUwU <90349878+ErickAlanesUwU@users.noreply.github.com>
2022-02-25 12:16:08 -0400

    Add files via upload

---
+++ Practica_Colas/Practica_Colas/Libreria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Colas
+{
+    class Libreria
+    {
+        //public int Price { get; set; }
+        public string Product { get; set; }
+
+        public Libreria(/*int price,*/ string product)
+        {
+            //Price = price;
+            Product = product;
+        }
+    }
+}
